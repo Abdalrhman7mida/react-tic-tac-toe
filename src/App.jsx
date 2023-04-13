29ef720be7ac92988c4f8ee87a9bd51e12fe159e
Abdalrhman Hamida <96482182+Abdalrhman7mida@users.noreply.github.com>
2023-04-13 18:08:28 +0200

    Styling for the Button
    - [x] Add styling for the button. [X, O]

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Button from './components/Button/Button'
 
 function App() {
 
   return (
     <div className="App">
-      
+      <Button />
     </div>
   )
 }
