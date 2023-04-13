fbd10e7c6bb30736ac3b18afd73ab10eea1472ea
Abdalrhman Hamida <96482182+Abdalrhman7mida@users.noreply.github.com>
2023-04-14 00:25:47 +0200

    Add Routes
    - [x] Add new Routes
        - Route for the `HomeScreen`.
        - Route for the 'tic tac toe` game.
        - Route to `NotFoundScreen`.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
+
 import Button from './components/Button/Button'
+import NavBar from './components/NavBar/NavBar';
+
+import Home from './screens/HomeScreen';
+import NotFoundScreen from './screens/NotFoundScreen';
+
 
 function App() {
 
   return (
     <div className="App">
-      <Button />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="tic-tac-toe" element={<Button />} />
+          <Route path="*" element={<NotFoundScreen />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
