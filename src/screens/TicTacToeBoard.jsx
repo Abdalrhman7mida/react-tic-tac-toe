3ba454a47dd74d64d3c596e6271dbfcc906c5996
Abdalrhman Hamida <96482182+Abdalrhman7mida@users.noreply.github.com>
2023-04-15 15:39:34 +0200

    Declaring a winner
    - add a helper function called `calculateWinner `that takes an array of 9 squares, checks for a winner and returns 'X', 'O', or null as appropriate.
    - Indicating the status we have reached through checking if there is a winner or not yet.

--- src/screens/TicTacToeBoard.jsx
+++ src/screens/TicTacToeBoard.jsx
@@ -7,6 +7,8 @@ function TicTacToeBoard() {
     const [squares, setSquares] = useState(Array(9).fill(null));
 
     function handleClick(i) {
+        if (calculateWinner(squares) || squares[i]) return;
+
         const nextSquares = squares.slice();
         if (squares[i]) return;
         xIsNext ? nextSquares[i] = "x" : nextSquares[i] = "o";
@@ -15,8 +17,13 @@ function TicTacToeBoard() {
         setxIsNext(!xIsNext);
     }
 
+    const winner = calculateWinner(squares);
+    let status;
+    winner ? status = `Winner: ${winner}`:  status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+
     return (
         <>
+            <div className="status">{status}</div>
             <div className="board-row">
                 <Square value={squares[0]} onSquareClick={() => { handleClick(0) }} />
                 <Square value={squares[1]} onSquareClick={() => { handleClick(1) }} />
@@ -34,5 +41,25 @@ function TicTacToeBoard() {
             </div>
         </>
     )
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (let i = 0; i<lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] && squares[a])
+            return squares[a];
+    }
+    return null;
 }
 export default TicTacToeBoard;
