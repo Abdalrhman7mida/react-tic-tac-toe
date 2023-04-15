c6905384b322db87a118382458d67e01df19b700
Abdalrhman Hamida <96482182+Abdalrhman7mida@users.noreply.github.com>
2023-04-15 17:10:34 +0200

    Optimization
    - Instead of computing the length over each iteration, i have stored it previously.
    
    - checking if there are any null squares remaining in the squares array.
    
    - Using object destructing to toggle the value of xIsNext, Instead of using setxIsNext(!xIsNext),
    
    - Avoid using the Array.slice() method by creating a new array using the spread operator.
    
    - Simplify the status variable using ternary operator.
    
    -  using React.memo() to optimize the rendering of the Square component.
    we use UseMemo if:
    
    - the Square component does not depend on any props or state, and will not change after it has been rendered.
        - in our state => By memoizing
          buttonContent, we can prevent
          unnecessary re-renders of the
          Square component when the value
          prop doesn't change.
    
    - Simplify the rendering of the Square components by using the Array.map() method.

--- src/components/Button/Square.jsx
+++ src/components/Button/Square.jsx
@@ -1,13 +1,16 @@
-import {React,useState} from "react";
+import {React,useMemo} from "react";
 import './Square.css'
 
 export function Square({value, onSquareClick}) {
+    const buttonContent = useMemo(() => {
+        return value ? value: "";
+    }, [value])
     return (
         <button 
         className="square"
         onClick={onSquareClick} 
         >
-            {value}
+            {buttonContent}
         </button>
     );
 }
--- src/screens/TicTacToeBoard.jsx
+++ src/screens/TicTacToeBoard.jsx
@@ -9,38 +9,33 @@ function TicTacToeBoard() {
     function handleClick(i) {
         if (calculateWinner(squares) || squares[i]) return;
 
-        const nextSquares = squares.slice();
+        const nextSquares = [...squares];
         if (squares[i]) return;
         xIsNext ? nextSquares[i] = "x" : nextSquares[i] = "o";
 
         setSquares(nextSquares);
-        setxIsNext(!xIsNext);
+        setxIsNext(prevValue => !prevValue);
     }
 
     const winner = calculateWinner(squares);
-    let status;
-    winner ? status = `Winner: ${winner}`:  status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+    const status = winner ? `Winner: ${winner}` : `Next player: ${xIsNext ? 'X' : 'O'}`;
 
     return (
         <>
-            <div className="status">{status}</div>
-            <div className="board-row">
-                <Square value={squares[0]} onSquareClick={() => { handleClick(0) }} />
-                <Square value={squares[1]} onSquareClick={() => { handleClick(1) }} />
-                <Square value={squares[2]} onSquareClick={() => { handleClick(2) }} />
-            </div>
-            <div className="board-row">
-                <Square value={squares[3]} onSquareClick={() => { handleClick(3) }} />
-                <Square value={squares[4]} onSquareClick={() => { handleClick(4) }} />
-                <Square value={squares[5]} onSquareClick={() => { handleClick(5) }} />
-            </div>
-            <div className="board-row">
-                <Square value={squares[6]} onSquareClick={() => { handleClick(6) }} />
-                <Square value={squares[7]} onSquareClick={() => { handleClick(7) }} />
-                <Square value={squares[8]} onSquareClick={() => { handleClick(8) }} />
-            </div>
-        </>
-    )
+        <div className="status">{status}</div>
+        {[0, 3, 6].map((start) => (
+          <div key={start} className="board-row">
+            {squares.slice(start, start + 3).map((value, index) => (
+              <Square
+                key={start + index}
+                value={value}
+                onSquareClick={() => handleClick(start + index)}
+              />
+            ))}
+          </div>
+        ))}
+      </>
+    );
 }
 
 function calculateWinner(squares) {
@@ -55,10 +50,16 @@ function calculateWinner(squares) {
         [2, 4, 6],
     ];
 
-    for (let i = 0; i<lines.length; i++) {
+    const len = lines.length;
+    for (let i = 0; i < len; i++) {
         const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] && squares[a])
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] && squares[a]){
             return squares[a];
+        }
+        // i face a problem with the follwing checking, it's not working properly if there is a winner with the last play with no any null positions last.   
+        // else if (!squares.includes(null)) {
+        //     return "draw";
+        // }
     }
     return null;
 }
